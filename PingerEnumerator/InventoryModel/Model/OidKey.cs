94bd98f4f15a97bf351be6299a92db643c4e8f55
Dauren Amanbayev <daur_aman@mail.ru>
2016-07-20 16:15:00 +0600

    Some changes with inventory model

---
+++ PingerEnumerator/InventoryModel/Model/OidKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryModel.Model
+{
+    public class OidKey
+    {
+        [Key]
+        public int Id { get; set; }
+        //уникальность ключа в таблице, добавить!!!
+        [Index("IX_UniqueOidKey", 1, IsUnique = true), Required, MaxLength(100)]
+        public string Key { get; set; }
+        [Required, MaxLength(100)]
+        public string Description { get; set; }
+        [Index("IX_UniqueOidKey", 2, IsUnique = true), Required]
+        public virtual Template Template { get; set; }
+    }
+}
