00d7d4d06d79106ee4fbeab7ec176037d5a4bea8
Dauren Amanbayev <daur_aman@mail.ru>
2016-07-20 16:12:40 +0600

    Changes with Detail model

--- PingerEnumerator/InventoryModel/Model/Detail.cs
+++ PingerEnumerator/InventoryModel/Model/Detail.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryModel.Model
 {
+    //сдвоенный ключ для опроса устройств
     public class Detail
     {
         public int Id { get;set;}
-        public virtual DataKey DataKey { get; set; }
+        [Index("IX_UniqueDetails", 1, IsUnique = true)]
+        public virtual OidKey DataKey { get; set; }
+        public DateTime CollectDate { get; set; }
         public string Properties { get; set; }
+        [Index("IX_UniqueDetails", 2, IsUnique = true)]
         public virtual Node Node { get; set; }
     }
 }
