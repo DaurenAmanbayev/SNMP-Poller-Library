f3ee665fa0a889ca35750a7ce9aba7e56e82e11f
Dauren Amanbayev <daur_aman@mail.ru>
2016-07-20 16:13:21 +0600

    Added Poller model

---
+++ PingerEnumerator/InventoryModel/Model/Poller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryModel.Model
+{
+    public class Poller
+    {
+        //для планирования поллинга устройств
+    }
+}
