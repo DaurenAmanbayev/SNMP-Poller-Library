9cd7b7117ac26b1ddfa06b6a26775631950c0cb2
Dauren Amanbayev <daur_aman@mail.ru>
2016-07-21 15:10:37 +0600

    Add GUI client

---
+++ PingerEnumerator/InventoryGUI/Main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace InventoryGUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeViewNodes = new System.Windows.Forms.TreeView();
+            this.listViewDetails = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(569, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeViewNodes);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewDetails);
+            this.splitContainer1.Size = new System.Drawing.Size(569, 420);
+            this.splitContainer1.SplitterDistance = 189;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // treeViewNodes
+            // 
+            this.treeViewNodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewNodes.Location = new System.Drawing.Point(0, 0);
+            this.treeViewNodes.Name = "treeViewNodes";
+            this.treeViewNodes.Size = new System.Drawing.Size(189, 420);
+            this.treeViewNodes.TabIndex = 0;
+            // 
+            // listViewDetails
+            // 
+            this.listViewDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewDetails.Location = new System.Drawing.Point(0, 0);
+            this.listViewDetails.Name = "listViewDetails";
+            this.listViewDetails.Size = new System.Drawing.Size(376, 420);
+            this.listViewDetails.TabIndex = 0;
+            this.listViewDetails.UseCompatibleStateImageBehavior = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 444);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeViewNodes;
+        private System.Windows.Forms.ListView listViewDetails;
+    }
+}
+
